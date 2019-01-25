33410d4e967b4185867e67b77443fa4d5404e580
carolcesp <carolinacespedes90@gmail.com>
2019-01-25 13:23:38 +0100

    mv build docs and added github pages

---
+++ docs/precache-manifest.aaad737604eaa8b1c78017c4ac250627.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f1c515668bc17f21820",
+    "url": "./static/css/main.35537889.chunk.css"
+  },
+  {
+    "revision": "2f1c515668bc17f21820",
+    "url": "./static/js/main.2f1c5156.chunk.js"
+  },
+  {
+    "revision": "9c764f7b5341bc745d08",
+    "url": "./static/js/1.9c764f7b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "e497aa07d706ee4c3ce449bf71008848",
+    "url": "./static/media/gryffindor.e497aa07.png"
+  },
+  {
+    "revision": "b511b9480d90c8a476bee0f90ab60908",
+    "url": "./static/media/hufflepuff.b511b948.png"
+  },
+  {
+    "revision": "f386438dd442658fe4b93e168ff17c7f",
+    "url": "./static/media/ravenclaw.f386438d.png"
+  },
+  {
+    "revision": "a8df18013d035b5d9e98c08b41605388",
+    "url": "./static/media/slytherin.a8df1801.png"
+  },
+  {
+    "revision": "1f9efcef83ff7b33f5eafb3996bbcc32",
+    "url": "./index.html"
+  }
+];
